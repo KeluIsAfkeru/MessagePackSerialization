aba0cf7421907b0f37b8e4e22ebc16fbf395fcae
Afkeru <451345424@qq.com>
2023-08-16 03:56:52 +0800

    添加项目文件。

---
+++ MemoryManager.cs
@@ -0,0 +1,127 @@
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SerializationBench
+{
+    // 一个通用的内存管理器，用于管理非托管内存块
+    public unsafe class MemoryManager<M> : IDisposable where M : unmanaged
+    {
+        public MemoryBlock memoryBlock;   // 内存块
+        private int currentPosition;       // 当前写入位置
+        private readonly int chunkSize;  // 内存块扩增的大小，可以根据需求进行调整
+        private bool disposed = false;     // 标记是否已经释放
+
+        // 构造函数，参数为内存块的初始大小
+        public MemoryManager(int initialSize)
+        {
+            if (initialSize <= 0) throw new ArgumentOutOfRangeException(nameof(initialSize));
+            memoryBlock = new MemoryBlock(initialSize * sizeof(M));
+            currentPosition = 0;
+            chunkSize = 1024 * sizeof(M);
+        }
+
+        public void WriteData<T>(T data) where T : unmanaged
+        {
+            if (disposed) throw new ObjectDisposedException("MemoryManager");
+
+            while (currentPosition * sizeof(M) + sizeof(T) > memoryBlock.Size)
+            {
+                memoryBlock.Resize(memoryBlock.Size + chunkSize);
+            }
+
+            *(T*)(memoryBlock.Block + currentPosition * sizeof(M)) = data;
+            currentPosition += sizeof(T) / sizeof(M);
+            memoryBlock.UsedSize = Math.Max(memoryBlock.UsedSize, currentPosition * sizeof(M));  // 更新已使用的内存长度
+        }
+
+        public unsafe void WriteData(string data)
+        {
+            if (disposed) throw new ObjectDisposedException("MemoryManager");
+
+            int byteCount = Encoding.UTF8.GetByteCount(data);
+
+            while (currentPosition + byteCount > memoryBlock.Size)
+            {
+                memoryBlock.Resize(memoryBlock.Size + chunkSize);
+            }
+
+            fixed (char* pStr = data)
+            {
+                byte* pDest = memoryBlock.Block + currentPosition;
+                Encoding.UTF8.GetBytes(pStr, data.Length, pDest, byteCount);
+            }
+            currentPosition += byteCount;
+            memoryBlock.UsedSize = Math.Max(memoryBlock.UsedSize, currentPosition * sizeof(M));  // 更新已使用的内存长度
+        }
+
+
+        // 从内存块中读取数据
+        public T ReadData<T>(int position) where T : unmanaged
+        {
+            if (disposed) throw new ObjectDisposedException("MemoryManager");
+            int realPosition = position * sizeof(M);
+            if (realPosition < 0 || realPosition >= currentPosition * sizeof(M)) throw new ArgumentOutOfRangeException(nameof(position));
+
+            // 从指定位置读取数据
+            T data = *(T*)(memoryBlock.Block + realPosition);
+            return data;
+        }
+
+        public unsafe string ReadString(int startPosition, int byteCount)
+        {
+            if (disposed) throw new ObjectDisposedException("MemoryManager");
+            if (startPosition < 0 || startPosition + byteCount > currentPosition * sizeof(M)) throw new ArgumentOutOfRangeException(nameof(startPosition));
+
+            byte* pSrc = memoryBlock.Block + startPosition;
+            int charCount = Encoding.UTF8.GetMaxCharCount(byteCount);
+            char* pStr = stackalloc char[charCount];
+
+            charCount = Encoding.UTF8.GetChars(pSrc, byteCount, pStr, charCount);
+
+            return new string(pStr, 0, charCount);
+        }
+
+        // 释放内存块
+        public void ReleaseMemory()
+        {
+            if (disposed) throw new ObjectDisposedException("MemoryManager");
+
+            memoryBlock.Release();
+            memoryBlock = null;
+            currentPosition = 0;
+        }
+
+        // Dispose方法，用于释放资源
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // 释放托管资源
+                    memoryBlock?.Release();
+                }
+                // 释放非托管资源
+                memoryBlock = null;
+                currentPosition = 0;
+                disposed = true;
+            }
+        }
+
+        // 析构函数，当垃圾回收器回收对象时会自动调用
+        ~MemoryManager()
+        {
+            // 不要改变此代码。 把清理代码放在 'Dispose(bool disposing)' 方法中
+            Dispose(disposing: false);
+        }
+
+        // IDisposable接口的Dispose方法，手动释放资源
+        public void Dispose()
+        {
+            // 不要改变此代码。 把清理代码放在 'Dispose(bool disposing)' 方法中
+            Dispose(disposing: true);
+            // 通知垃圾回收器不要调用析构函数
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using MessagePack;
+using System.Diagnostics;
+using System.Collections.Concurrent;
+using System.Text;
+
+namespace SerializationBench
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 创建10000个Cell实例
+            Console.WriteLine("加载初始数据中...");
+            var count = 1;
+
+            var cells = new Cell[count];
+            ObjectPool<Cell> pool = new ObjectPool<Cell>(count);
+
+            Parallel.For(0, count, i =>
+            {
+                var cell = pool.Get();
+                cells[i] = cell;
+                // 不要立即返回对象到池中，因为我们仍然在使用它。
+                // 当完成后或在适当的时机，可以调用 pool.Return(cell)。
+            });
+
+            Console.WriteLine("加载完成，开始使用自编内存管理器序列化");
+
+            #region 使用自研的内存管理器进行序列化与反序列化操作
+            // 创建内存管理器
+            MemoryManager<byte> memoryManager = new MemoryManager<byte>(1024);
+
+            // 计时器开始
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            // 写入Cell列表的长度
+            memoryManager.WriteData((uint)cells.Length);
+
+            // 将每个Cell的属性依次写入MemoryManager
+            foreach (Cell cell in cells)
+            {
+                memoryManager.WriteData(cell.X);
+                memoryManager.WriteData(cell.Y);
+                memoryManager.WriteData(cell.R);
+                memoryManager.WriteData(cell.ID);
+                memoryManager.WriteData(cell.Color);
+                memoryManager.WriteData((uint)Encoding.UTF8.GetByteCount(cell.Name));
+                memoryManager.WriteData(cell.Name);
+            }
+
+            // 计时器停止
+            stopwatch.Stop();
+            Console.WriteLine($"Serialization and writing time: {stopwatch.ElapsedMilliseconds} ms");
+
+            // 计时器重新开始
+            stopwatch.Restart();
+
+            // 从MemoryManager中读取Cell列表的长度
+            int cellCount = (int)memoryManager.ReadData<uint>(0);
+
+            // 根据长度和之前的类型顺序反序列化Cell列表
+            List<Cell> deserializedCells = new List<Cell>();
+            int currentPosition = sizeof(uint);
+            for (int i = 0; i < cellCount; i++)
+            {
+                double x = memoryManager.ReadData<float>(currentPosition);
+                currentPosition += sizeof(float);
+
+                double y = memoryManager.ReadData<float>(currentPosition);
+                currentPosition += sizeof(float);
+
+                double r = memoryManager.ReadData<float>(currentPosition);
+                currentPosition += sizeof(float);
+
+                int id = (int)memoryManager.ReadData<uint>(currentPosition);
+                currentPosition += sizeof(uint);
+
+                byte color = memoryManager.ReadData<byte>(currentPosition);
+                currentPosition += sizeof(byte);
+
+                int nameByteCount = (int)memoryManager.ReadData<uint>(currentPosition);
+                currentPosition += sizeof(uint);
+
+                string name = memoryManager.ReadString(currentPosition, nameByteCount);
+                currentPosition += nameByteCount;
+
+                //deserializedCells.Add(new Cell(x, y, r, id, color, name));
+            }
+
+            // 计时器停止
+            stopwatch.Stop();
+            Console.WriteLine($"Reading and deserialization time: {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"size: {memoryManager.memoryBlock.UsedSize} byte");
+
+            #endregion
+
+            #region msgpack
+            //// 配置MessagePack序列化器
+            Console.WriteLine("下面使用msgpack序列化");
+            var options = MessagePackSerializerOptions.Standard.WithCompression(MessagePackCompression.Lz4BlockArray);
+
+            stopwatch.Reset();
+
+            // 开始计时序列化过程
+            stopwatch.Start();
+
+            // 将Cell实例序列化为字节数组
+            var serialized = MessagePackSerializer.Serialize(cells, options);
+
+            // 停止计时
+            stopwatch.Stop();
+
+            Console.WriteLine($"Serialization took {stopwatch.ElapsedMilliseconds} ms");
+            Console.WriteLine($"Pack size {serialized.Length} byte");
+
+            // 重置计时器
+            stopwatch.Reset();
+
+            // 开始计时反序列化过程
+            stopwatch.Start();
+
+            // 将字节数组反序列化为Cell实例
+            var deserialized = MessagePackSerializer.Deserialize<List<Cell>>(serialized, options);
+
+            // 停止计时
+            stopwatch.Stop();
+
+            Console.WriteLine($"Deserialization took {stopwatch.ElapsedMilliseconds} ms");
+
+            // 打印结果
+            Console.WriteLine($"Original count: {count}");
+            Console.WriteLine($"Deserialized count: {deserialized.Count}");
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+
+    public class ObjectPool<T> where T : new()
+    {
+        private ConcurrentBag<T> _objects = new ConcurrentBag<T>();
+        private int _capacity;
+
+        public ObjectPool(int capacity)
+        {
+            _capacity = capacity;
+        }
+
+        public T Get()
+        {
+            if (_objects.TryTake(out T item))
+                return item;
+            return new T();
+        }
+
+        public void Return(T item)
+        {
+            if (_objects.Count < _capacity)
+                _objects.Add(item);
+        }
+    }
+
+}
